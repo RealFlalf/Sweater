2d7ce533a57aa95177173521386db8e57ef3ddd5
Max <realfalf@gmail.com>
2020-04-14 22:12:04 +0300

    captcha add

--- src/main/java/com/example/sweater/domain/User.java
+++ src/main/java/com/example/sweater/domain/User.java
@@ -22,10 +22,6 @@ public class User implements UserDetails {
     @NotBlank(message = "Password cannot be empty")
     private String password;
 
-    @Transient
-    @NotBlank(message = "Password confirm cannot be empty")
-    private String password2;
-
     private boolean active;
 
     @Email(message = "email is not correct")
@@ -121,13 +117,5 @@ public class User implements UserDetails {
 
     public void setActivationCode(String activationCode) {
         this.activationCode = activationCode;
-    }
-
-    public String getPassword2() {
-        return password2;
-    }
-
-    public void setPassword2(String password2) {
-        this.password2 = password2;
     }
 }
